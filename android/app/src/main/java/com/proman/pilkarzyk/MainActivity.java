031cd12f160fb7fcc6a53198b380d699c86aa342
jdrozd <jakub.drozd@proman.com.pl>
2023-11-29 14:24:24 +0100

    capacitor, ionic

---
+++ android/app/src/main/java/com/proman/pilkarzyk/MainActivity.java
@@ -0,0 +1,5 @@
+package com.proman.pilkarzyk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proman.pilkarzyk',
+  appName: 'pilkarzyk',
+  webDir: 'dist/pilkarzyk',
+  server: {
+    url: 'http://192.168.88.20:4200',
+    cleartext: true,
+    androidScheme: 'https'
+  }
+};
+
+export default config;

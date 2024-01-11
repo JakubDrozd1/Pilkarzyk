ae4b3b73211e2a17a463bfcbfbac5953f5ffdd23
jdrozd <jakub.drozd@proman.com.pl>
2024-01-11 08:14:53 +0100

    JABALL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.proman.pilkarzyk',
-  appName: 'pilkarzyk',
+  appName: 'JABALL',
   webDir: 'dist/pilkarzyk',
   server: {
     url: 'https://jabol.manowski.pl', //http://192.168.1.114:4200  http://192.168.88.20:4200

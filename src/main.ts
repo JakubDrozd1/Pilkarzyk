2f795eb479f43d6d12700b2c9d4dffe9bd7767e6
jdrozd <jakub.drozd@proman.com.pl>
2024-04-25 15:58:31 +0200

    Dodanie skalowania avatara, poprawa odświezania tlumaczenia

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,11 @@ export function tokenGetter() {
 }
 
 export function httpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http)
+  return new TranslateHttpLoader(
+    http,
+    '/assets/i18n/',
+    '.json?cb=' + new Date().getTime()
+  )
 }
 
 bootstrapApplication(AppComponent, {
